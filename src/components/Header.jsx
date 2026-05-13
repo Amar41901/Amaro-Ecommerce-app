fc53f6d46ac7397b55b083f21cd68e2c5eef9a89
Amarvalar41901 <amar@valardigital.com>
2026-05-13 23:17:47 +0530

    Files added successfully

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import '../styles/Header.css'
+
+function Header(props) {
+    return (
+        <div className='header'>
+            <div className='logo'>
+                <img src="/assets/images/website-logo.webp" alt="Invalid path" />
+            </div>
+            <nav>
+                <Link to='/'>Dashboard</Link>
+                <Link to='/cart'>Cart</Link>
+                <Link to='/login'>Login</Link>
+                <Link to='/signin'>Sign In</Link>
+            </nav>
+        </div>
+    );
+}
+
+export default Header;
